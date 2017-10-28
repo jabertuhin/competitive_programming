04d5316a4077a84fc9fbe1c8e6efc17e79c55d62
jabertuhin <jabertuhin@gmail.com>
2017-10-28 10:19:54 +0600

    Add files via upload

---
+++ solved problems/Spoj/EKO - Eko.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary(int a[],int siz,long long key){
+    int l = 0,r = a[siz -1];
+    int mid;
+    while(r - l>1){
+    	mid = (l+r)/2;
+    	long long sum = 0;
+    	for(int count1 = 0;count1<siz;count1++){
+        	if(a[count1] > mid)
+            sum += (a[count1]-mid);
+        }
+        if(sum >= key){
+            l = mid;
+        }else{
+            r = mid;
+        }
+     }
+     return (l+r)/2;
+ }
+ 
+int main() {
+	int N;
+    long long M;
+    scanf("%d %llu", &N, &M);
+    int arr[N];
+    for(int count1 = 0;count1<N;count1++){
+        scanf("%d", &arr[count1]);
+    }
+    sort(arr,arr+N);
+    int rs = binary(arr,N,M);
+    cout<<rs<<endl;
+    return 0;
+}
